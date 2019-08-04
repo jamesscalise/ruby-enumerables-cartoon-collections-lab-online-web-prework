96133b6fa4bc6ec36440e88ad4656e92c56189aa
James Scalise <jamescscalise@gmail.com>
2019-08-04 19:49:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.collect{|name| 
+  array.collect{|name| "#{name.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
