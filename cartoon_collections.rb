1dae494abc48ae4f6ade98812e8f3d0cc66b23a0
James Scalise <jamescscalise@gmail.com>
2019-08-04 20:03:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find{|word| cheese_types.include(word)}
+  array.find{|word| cheese_types.include?(word)}
 end
