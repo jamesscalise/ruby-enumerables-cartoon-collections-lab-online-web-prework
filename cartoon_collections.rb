73d0448aa9aa58e03aff48d0361c30ad905e7e1e
James Scalise <jamescscalise@gmail.com>
2019-08-04 19:47:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|name, index|
+  
+    puts "#{index+1} #{name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
 end
 
 def long_planeteer_calls# code an argument here
